3856fde086b0ba34017bff6609cf3dcddf3e3ce3
aoki-h-jp <aoki.hirotaka.biz@gmail.com>
2023-06-14 01:52:40 +0900

    Hotfix can't pip install

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
-    name="binance-liquidation-feeder",
-    version="1.0.1",
-    packages=["."],
-    description="Notify liquidation on Binance.",
-    author="aoki-h-jp",
-    author_email="aoki.hirotaka.biz@gmail.com",
-    license="MIT",
+    name='binance-liquidation-feeder',
+    version="1.0.0",
+    description='Notify liquidation on Binance.',
+    install_requires=[],
+    author='aoki-h-jp',
+    author_email='aoki.hirotaka.biz@gmail.com',
+    license='MIT',
+    packages=find_packages(
+        include=['feeder'],
+        exclude=['img']
+    ),
 )
