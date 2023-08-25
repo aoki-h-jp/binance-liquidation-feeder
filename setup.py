186c87c6e37027bd505f3bb4d290c257d7ade698
Hirotaka Aoki <113173839+aoki-h-jp@users.noreply.github.com>
2023-08-26 01:49:22 +0900

    fix setup error

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="binance-liquidation-feeder",
     version="1.0.1",
-    packages=["binance_liquidation_feeder"],
+    packages=["."],
     description="Notify liquidation on Binance.",
     author="aoki-h-jp",
     author_email="aoki.hirotaka.biz@gmail.com",
